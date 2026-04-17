7ff73353eb7604a88abc3450fd1e3c9f603e0a49
unknown <06257392306@edu.udesc.br>
2026-04-16 21:44:19 -0300

    Exercicio 1 classe Aluno e respectivo testes

--- src/exercicio1oo/classes/TestaAluno.java
+++ src/exercicio1oo/classes/TestaAluno.java
@@ -1,11 +1,12 @@
 package exercicio1oo.classes;
+
 import java.util.Scanner;
 
- class TestaAluno {
+class TestaAluno {
     static void main() {
         Scanner leitor = new Scanner(System.in);
 
-        Aluno novo= new Aluno();
+        Aluno novo = new Aluno();
 
         System.out.println("Nome: ");
         novo.nome = leitor.nextLine();
@@ -29,12 +30,12 @@ import java.util.Scanner;
         novo.nota4 = leitor.nextInt();
 
         System.out.println("Dados do Aluno");
-        System.out.println("Nome: "+ novo.nome);
-        System.out.println("Matricula: "+ novo.matricula);
-        System.out.println("Idade: "+ novo.idade);
-        System.out.println("Nota 1: "+ novo.nota1);
-        System.out.println("Nota 2: "+ novo.nota2);
-        System.out.println("Nota 3: "+ novo.nota3);
-        System.out.println("Nota 4: "+ novo.nota4);
+        System.out.println("Nome: " + novo.nome);
+        System.out.println("Matricula: " + novo.matricula);
+        System.out.println("Idade: " + novo.idade);
+        System.out.println("Nota 1: " + novo.nota1);
+        System.out.println("Nota 2: " + novo.nota2);
+        System.out.println("Nota 3: " + novo.nota3);
+        System.out.println("Nota 4: " + novo.nota4);
     }
 }
